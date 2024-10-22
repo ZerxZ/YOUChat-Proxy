b84f0a5c9622a81088abf1e696a1273fa681812d
被遗忘的记忆 <32097237+YIWANG-sketch@users.noreply.github.com>
2024-10-23 01:36:40 +0800

    add happyapi

--- provider.mjs
+++ provider.mjs
@@ -1,5 +1,6 @@
 import YouProvider from './you_providers/youProvider.mjs';
 import PerplexityProvider from './perplexity_providers/perplexityProvider.mjs';
+import HappyApiProvider from './happyapi_providers/happyApi.mjs';
 import { config as youConfig } from './config.mjs';
 import { config as perplexityConfig } from './perplexityConfig.mjs';
 
@@ -15,8 +16,11 @@ class ProviderManager {
             case 'perplexity':
                 this.provider = new PerplexityProvider(perplexityConfig);
                 break;
+            case 'happyapi':
+                this.provider = new HappyApiProvider();
+                break;
             default:
-                throw new Error('Invalid ACTIVE_PROVIDER. Use "you" or "perplexity".');
+                throw new Error('Invalid ACTIVE_PROVIDER. Use "you", "perplexity", or "happyapi".');
         }
 
         console.log(`Initialized with ${activeProvider} provider.`);
