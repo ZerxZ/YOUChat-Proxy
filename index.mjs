561f70eda397c5fc0899b452d00f77b9f5210321
被遗忘的记忆 <32097237+YIWANG-sketch@users.noreply.github.com>
2024-10-26 18:54:53 +0800

    add claude-3-5-sonnet-20241022

--- index.mjs
+++ index.mjs
@@ -29,8 +29,11 @@ const availableModels = [
     "zephyr",
 ];
 const modelMappping = {
+    "claude-3-5-sonnet-latest": "claude_3_5_sonnet",
+    "claude-3-5-sonnet-20241022": "claude_3_5_sonnet",
     "claude-3-5-sonnet-20240620": "claude_3_5_sonnet",
     "claude-3-20240229": "claude_3_opus",
+    "claude-3-opus-20240229": "claude_3_opus",
     "claude-3-sonnet-20240229": "claude_3_sonnet",
     "claude-3-haiku-20240307": "claude_3_haiku",
     "claude-2.1": "claude_2",
